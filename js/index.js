8029689a1071fa1446baa5bc92b8f57653c87ca8
邓佳艳 <1143463819@qq.com>
2016-06-14 00:20:24 +0800

    wangyi

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(function(){
+	$(function($){
+		$('#fullpage').fullpage({
+			navigation: true,
+			navigationTooltips: ['首页','央视报道','视觉','交互','皮肤','功能','待办邮件','联系人邮件','科技','连接易信','马上体验'],
+		});
+	})
+})
